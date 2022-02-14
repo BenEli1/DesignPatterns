cedf88d77d88c250e31b52a95fa1d2f7029ec407
Ben Eli <benelashvili@gmail.com>
2022-02-14 21:19:21 +0200

    added singleton.h and factory.h

--- main.cpp
+++ main.cpp
@@ -6,25 +6,50 @@
 #include <vector>
 #include "prototype.h"
 #include "abstractFactory.h"
+#include "factory.h"
+#include "singleton.h"
 #include <map>
 using namespace std;
 
 int main(){
-//prototype check
-//    Circle* c1 =  new Circle(5,5,12);
-//    Circle* c2 = c1.clone();
-
 //abstract factory check
-ConcreteFactory1* f1 = new ConcreteFactory1();
-Client* c1 = new Client(f1);
-ConcreteFactory2* f2 = new ConcreteFactory2();
-Client* c2 = new Client(f2);
-ProductA* a1 = c1->createSword();
-ProductB* b1 =c1->createShield();
-ProductA* a2 = c2->createSword();
-ProductB* b2 =c2->createShield();
-cout<<"Client Number 1 Sword:  "<<a1->print()<<"\t  Shield: "<<b1->print()<<endl;
-cout<<"Client Number 2 Sword:  "<<a2->print()<<"\t  Shield: "<<b2->print()<<endl;
+//ConcreteFactory1* f1 = new ConcreteFactory1();
+//Client* c1 = new Client(f1);
+//ConcreteFactory2* f2 = new ConcreteFactory2();
+//Client* c2 = new Client(f2);
+//ProductA* a1 = c1->createSword();
+//ProductB* b1 =c1->createShield();
+//ProductA* a2 = c2->createSword();
+//ProductB* b2 =c2->createShield();
+//cout<<"Client Number 1 Sword:  "<<a1->print()<<"\t  Shield: "<<b1->print()<<endl;
+//cout<<"Client Number 2 Sword:  "<<a2->print()<<"\t  Shield: "<<b2->print()<<endl;
+//delete f1;
+//delete f2;
+//delete c1;
+//delete c2;
+//delete a1;
+//delete a2;
+//delete b1;
+//delete b2;
+
+
+//factory
+//int x = 0;
+//char* input;
+//map<const char*,WorkerFactory> factoryMap;
+//WorkerFactory* taFactory = new TAFactory();
+//factoryMap.insert(std::pair<const char*,WorkerFactory*>("TA",taFactory));
+//while(x<10){
+//    cin >> input;
+//    if(input == "TA"){
+//       arrayWorker[x] =
+//    }
+//    x++;
+//}
+
+//singleton
+//    Singleton* s1 = Singleton::getInstance();
+//    Singleton* s2= Singleton::getInstance();
 
 
 return 0;
